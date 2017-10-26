ac6bc0ca8c68075d72e0b6d4b87828de43359df1
Carrington Jones <cjones6096@greenvilleschools.us>
2017-10-26 18:12:59 -0400

    allll doneeee

--- MixedLoopPractice.java
+++ MixedLoopPractice.java
@@ -55,13 +55,14 @@
             System.out.print(i + " ");
             
         }
-        
         System.out.println();
+        System.out.println();
+        
     } 
     
     public static void oneToThirtyW()
     {
-    int i = 1;
+        int i = 1;
        
     while (i <= 30) 
     {
@@ -69,8 +70,9 @@
         
         i = i + 1;
     }
-    
     System.out.println();
+    System.out.println();
+        
     } 
     
     public static void twentyToZeroF()
@@ -78,10 +80,13 @@
         for (int i = 20;  i >= 0; i = i - 1)
         {
             System.out.print(i + " ");
+            
         }
-        
         System.out.println();
+        System.out.println();
+        
     }
+    
     public static void twentyToZeroW()
     {
         int i = 20;
@@ -93,13 +98,139 @@
             i = i -1;
         }
         System.out.println();
-    }
-    public static void sumN()
-    {
-    }
-    
-    
+        System.out.println();
         
+    }
+    
+    public static int sumN(int n)
+    {
+        int i = 0;
+        int sum = 0;
+        
+        while (i <= n)
+        {
+            System.out.print(i + "  ");
+            
+            sum = sum + i;
+            
+            i = i + 1;
+        }
+        return sum;
+      
+    }
+    
+    public static void countMtoN(int m, int n)
+    {
+        System.out.println();
+        int i = m;
+        
+        while (i <= n)
+        {
+            System.out.print(i + " ");
+            
+            i = i + 1;
+        }
+        System.out.println();
+        System.out.println();
+        
+    }
+    
+    public static void iLoveJava(int n)
+    {
+        int i = 1;
+        
+        while (i <= n)
+        {
+            System.out.println(" I loveee Java.");
+            
+            i = i + 1;
+        }
+        System.out.println();
+        
+    }
+    
+    public static void divBy2and9()
+    { 
+        for (int i = 1; i <= 100; i = i + 1)
+        {
+            if (i % 2 == 0 && i % 9 == 0)
+            { 
+                System.out.print(i + " ");
+                
+            }
+        }
+        System.out.println();
+        System.out.println();
+        
+    }
+    
+    public static void divBy3and8()
+    {
+        int i = 1;
+        
+        while ( i <= 100)
+        {
+            if (i % 3 == 0 && i % 8 == 0)
+            {
+                System.out.print(i + " ");
+                
+            }
+            i = i + 1;
+            
+        }
+        System.out.println();
+        System.out.println();
+        
+    }
+    
+    public static int divBy5or7Sum()
+    { 
+        int i = 1;
+        int sum = 0;
+        
+        while (i <= 1000)
+        { 
+            if (i % 5 == 0 || i % 7 == 0)
+            {
+                System.out.print(i + " ");
+                
+                sum = sum + i;
+            }
+            i = i + 1;
+            
+        }
+        return sum;
+        
+    }
+    
+    public static void bizzBuzz(int n)
+    {
+    System.out.println();
+    System.out.println();
+    
+        int i = 1;
+        
+        while (i <= n)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+            System.out.println("BIZZ BUZZ");
+            }
+            else if (i % 3 == 0)
+            { 
+            System.out.println("BIZZ");
+            }
+            else if (i % 5 == 0)
+            {
+            System.out.println("BUZZ");
+            }
+            else 
+            {
+            System.out.println(i);
+            }
+            i = i + 1;
+    }
+    }
     // keep making methods here
     public static void main(String[] args)
     {
@@ -107,6 +238,15 @@
      oneToThirtyW();
      twentyToZeroF();
      twentyToZeroW();
+     System.out.println(sumN(10));
+     countMtoN(15, 20);
+     iLoveJava(15);
+     divBy2and9();
+     divBy3and8();
+     divBy5or7Sum();
+     bizzBuzz(25);
+     
+     
         
      // test your methods here
     }
